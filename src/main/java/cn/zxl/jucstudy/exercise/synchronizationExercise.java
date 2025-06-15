f005af4a0d33eb66c100d1c8db7375f2934c136f
Anasiotasio <Zxl2024903531@outlook.com>
2025-06-15 19:29:11 +0800

    Refactor synchronizationExercise to use Room class for thread-safe counter operations

--- src/main/java/cn/zxl/jucstudy/exercise/synchronizationExercise.java
+++ src/main/java/cn/zxl/jucstudy/exercise/synchronizationExercise.java
@@ -4,22 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j(topic = "c.synchronizationExercise")
 public class synchronizationExercise {
-    static int counter = 0;
-    static final Object room = new Object();//必须是实际存在的对象，是一个对象锁
+
+/*    static int counter = 0;
+    static final Object room = new Object();//必须是实际存在的对象，是一个对象锁*/
     public static void main(String[] args) throws InterruptedException {
+        Room room= new Room();
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 5000; i++) {
-                synchronized (room) {
-                    counter++;
-                }
+                room.increment();
             }
         },
                 "t1");
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 5000; i++) {
-                synchronized (room) {
-                    counter--;
-                }
+                room.decrement();
             }
         },
                 "t2");
@@ -27,7 +25,26 @@ public class synchronizationExercise {
         t2.start();
         t1.join();
         t2.join();
-        log.info("{}",counter);//会发现只能是0了，不再出现正负数
+        log.info("{}",room.getCounter());//会发现只能是0了，不再出现正负数
     }
 
+}
+
+class Room{
+    private static int counter = 0;
+    public  void increment() {
+        synchronized (this) {
+            counter++;
+        }
+    }
+    public void decrement() {
+        synchronized (this) {
+            counter--;
+        }
+    }
+    public int getCounter() {
+        synchronized (this) {
+            return counter;
+        }
+    }
 }
